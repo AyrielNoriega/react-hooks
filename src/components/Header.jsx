8d4daa237c19b7698253ac71d5c142914746bd45
AyrielNoriega <anoriega2310@gmail.com>
2023-04-20 21:43:03 -0500

    useContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import ThemeContext from "../context/ThemeContext";
 
 export const Header = () => {
 
     const [darkMode, setDarkMode] = useState(false);
 
+    const color = useContext(ThemeContext);
+    console.log(color);
     const onHandleClic = () => {
         setDarkMode(!darkMode);
     }
 
     return (
         <div>
-            <h1>Heacer</h1>
+            <h1 style={{ color }}>Header</h1>
             <button className="button" onClick={onHandleClic}>{darkMode ? 'Dark Mode': 'Light Mode'}</button>
         </div>
     )
